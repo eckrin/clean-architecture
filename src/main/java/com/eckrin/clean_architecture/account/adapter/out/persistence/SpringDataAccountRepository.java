0f32ef8ad3adb7df9f729deca706aa8685497255
eckrin <bsh51515@naver.com>
2024-05-22 14:39:32 +0900

    fix: 도메인 엔티티와 영속 엔티티 분리

--- src/main/java/com/eckrin/clean_architecture/account/adapter/out/persistence/SpringDataAccountRepository.java
+++ src/main/java/com/eckrin/clean_architecture/account/adapter/out/persistence/SpringDataAccountRepository.java
@@ -1,6 +1,6 @@
 package com.eckrin.clean_architecture.account.adapter.out.persistence;
 
-import com.eckrin.clean_architecture.account.domain.Account;
+import com.eckrin.clean_architecture.account.adapter.out.persistence.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SpringDataAccountRepository extends JpaRepository<Account, Long> {
