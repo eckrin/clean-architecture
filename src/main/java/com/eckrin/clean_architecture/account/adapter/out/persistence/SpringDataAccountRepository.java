24c60754b8f5aff58b8fc7a6f60e79a227eb233b
eckrin <bsh51515@naver.com>
2024-05-28 11:11:40 +0900

    feat: Spring Data JPA를 사용한 영속계층 repository 추가

--- src/main/java/com/eckrin/clean_architecture/account/adapter/out/persistence/SpringDataAccountRepository.java
+++ src/main/java/com/eckrin/clean_architecture/account/adapter/out/persistence/SpringDataAccountRepository.java
@@ -1,7 +1,7 @@
 package com.eckrin.clean_architecture.account.adapter.out.persistence;
 
-import com.eckrin.clean_architecture.account.adapter.out.persistence.entity.Account;
+import com.eckrin.clean_architecture.account.adapter.out.persistence.entity.AccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SpringDataAccountRepository extends JpaRepository<Account, Long> {
+interface SpringDataAccountRepository extends JpaRepository<AccountEntity, Long> {
 }
